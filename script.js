a32f1a7a2be1e132fdf119daa38267c5cc8bbb6d
leowilson <leobca2018@gmail.com>
2022-06-09 09:34:24 +0530

    add javascript file

---
+++ script.js
@@ -0,0 +1,13 @@
+let range=parseInt(prompt());
+let a = 0;
+let b = 1;
+console.log (a);
+for ( i = 0; i <=range; i++)
+{
+    // console.log(a)
+    c = a + b;
+    a = b;
+    b = c;
+    console.log(c)
+}
+  
